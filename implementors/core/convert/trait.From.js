56953d2ebb5ecda24caf394e2992924e5e3ed6f1
travis@localhost <travis@localhost>
2015-10-07 05:44:37 +0000

    Update documentation

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mpi'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='type' href='https://doc.rust-lang.org/nightly/libc/types/os/arch/c95/type.c_int.html' title='libc::types::os::arch::c95::c_int'>c_int</a>&gt; for <a class='enum' href='mpi/topology/enum.Threading.html' title='mpi::topology::Threading'>Threading</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='type' href='https://doc.rust-lang.org/nightly/libc/types/os/arch/c95/type.c_int.html' title='libc::types::os::arch::c95::c_int'>c_int</a>&gt; for <a class='enum' href='mpi/topology/enum.CommunicatorRelation.html' title='mpi::topology::CommunicatorRelation'>CommunicatorRelation</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='type' href='https://doc.rust-lang.org/nightly/libc/types/os/arch/c95/type.c_int.html' title='libc::types::os::arch::c95::c_int'>c_int</a>&gt; for <a class='enum' href='mpi/topology/enum.GroupRelation.html' title='mpi::topology::GroupRelation'>GroupRelation</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
